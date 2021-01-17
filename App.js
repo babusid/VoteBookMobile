d7ea950a9111c64b0d53ee8b5e88804abc73c5a6
William Hong <hongwil123@gmail.com>
2021-01-17 16:59:02 -0600

    Created all the graphical elements and stylesheets

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import BallotPlanner from './__screens__/BallotPlanner'
 import MapScreen from './__screens__/Map'
+import LoginScreen from './__screens__/Login.js'
 
 const rootStack = createStackNavigator();
 const rootTab = createBottomTabNavigator();
@@ -18,7 +19,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <rootStack.Navigator>
-        <rootStack.Screen component={rootTabNav()}/>
+        <rootStack.Screen component={LoginScreen} name="LoginScreen" />
       </rootStack.Navigator>
     </NavigationContainer>
   );
--- __screens__/Login.js
+++ __screens__/Login.js
@@ -1,13 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import rootStore from "../__components__/__redux__/store.js"
+import { StyleSheet, Text, View, ScrollView, Image, Button, Keyboard, KeyboardAvoidingView } from 'react-native';
+import { Card, Input } from 'react-native-elements';
+import rootStore from "../__components__/__redux__/store.js";
+import ImagePlaceholder from './Images/ImagePlaceholder.jpg';
 
-
+/**
+ * @brief
+ * 
+ * @returns
+ * 
+ */
 export default function LoginScreen(){
     return(
-        <View>
-            <Text>This is where the Login Screen should be exported from</Text>
-        </View>
+        <KeyboardAvoidingView behavior={"padding"}>
+        <ScrollView>
+            <Card>
+                <View style={styles.container}>
+                    <Image 
+                        style={{width: 150, height: 150}}
+                        resizeMode="contain"
+                        source={ImagePlaceholder} 
+                    /> 
+                    <Input
+                        style={{paddingTop: 40}}
+                        placeholder='Enter your zip code'
+                    />
+                </View>
+                <View style={styles.button}>
+                    <Button 
+                        color='blue' 
+                        title='Enter'
+                    />
+                </View>
+            </Card>
+        </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    button: {
+        padding: 15,
+    },
+});
