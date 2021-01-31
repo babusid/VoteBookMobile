5af5fec74f7c973d0a4ffd04da7283c44ed2eb32
Sully Mrkva <manlybros11@gmail.com>
2021-01-31 16:37:31 -0600

    updated comments

--- __components__/__redux__/Reducers/zipcodeReducer.js
+++ __components__/__redux__/Reducers/zipcodeReducer.js
@@ -1,4 +1,9 @@
 
+/**
+ * 
+ * @brief this function takes the dispatched state from the previous action file and checks if it is of type zipcodeDispatch.
+ * If this is true, it returns the payload of the action, which in this isntance would be the zipcode. 
+ */
 export function zipcodeReducer(state, action){
 if (action.type == 'zipcodeDispatch'){
     return action.payload;
