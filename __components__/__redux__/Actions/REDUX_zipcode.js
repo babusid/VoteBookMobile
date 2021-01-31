aa633fe8252da8be46f536d8e4fa324b2e4df889
Sully Mrkva <manlybros11@gmail.com>
2021-01-31 16:35:00 -0600

    added comments

--- __components__/__redux__/Actions/REDUX_zipcode.js
+++ __components__/__redux__/Actions/REDUX_zipcode.js
@@ -5,6 +5,7 @@ import { createAction } from '@reduxjs/toolkit'
  * 
  * @brief this function essentially takes in the input given from someone that is on the app. After that it uses 
  * the input to create a "state" of the data, AKA, what the current zipcode is. After this is defined, it exports
+ * it to the rootStore and stores it.
  */
 export function dispatchZipcode( input ){
 
@@ -15,6 +16,10 @@ export function dispatchZipcode( input ){
     rootStore.dispatch(zipcodeDispatch);   
 }
 
+/**
+ * @brief this function essentially just returns the zipcode that has been inputed. It takes the data from the rootstore
+ * and returns it in a string.
+ */
 export function getZipcode(){
   var state = rootStore.getState().zipcode;
 
