43b8915c9043c72466bee4e60bb88f7ac0801344
Sully Mrkva <manlybros11@gmail.com>
2021-01-31 13:22:21 -0600

    updated new reducer function

--- __components__/__redux__/reducer.js
+++ __components__/__redux__/reducer.js
@@ -3,11 +3,8 @@ import { rootstore, rootReducer } from store.js
 
 
 export function zipcodeReducer(state, action){
-if (typeof state === 'undefined'){
-    state = 0;
-}
-else {
- rootReducer(state, action);
+if (action.type == 'zipcodeDispatch'){
+    return action.payload;
 }
 
 }
