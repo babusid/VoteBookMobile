4f3a9433fcfb12edf8021f930c585d1ceb4616e0
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2021-02-01 14:09:09 -0600

    Created Error exceptions file

---
+++ __error.js
@@ -0,0 +1,8 @@
+/** @brief throw this exception for any function that is expecting an Array and does not get one*/
+export const ArrayThrowException = "Input parameter is not an array.";
+/**@brief throw this exception for any function that is expecting an object and does not get one */
+export const ObjectThrowException = "Input parameter is not an object.";
+/**@brief throw this exception for any function that is expecting a string and does not get one */
+export const StringThrowException = "Input parameter is not a String";
+/**@brief throw this exception for any function that is expecting an integer and does not get one */
+export const IntThrowException = "Input Parameter is not an integer";
