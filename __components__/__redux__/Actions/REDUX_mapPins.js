584cdc85204823ca696bb259b7f353c0d041e387
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2021-02-07 12:48:30 -0600

    babusid/issue6 (#10)
    * created initial filestructure
    
    * Created Redux api
    
    * Created Error exceptions file
    
    * created test for mapPins.js
    
    * linked reducer, added basic tests
    
    * Added Briefing

---
+++ __components__/__redux__/Actions/REDUX_mapPins.js
@@ -0,0 +1,27 @@
+import {rootStore} from '../store.js'
+import { ArrayThrowException } from '../../../__error'
+
+/**
+ * 
+ * @brief This function is what is called to dispatch the array of map pins to our store
+ * @param {Array} input The Array of Objects that are the pins for the map
+ * 
+ */
+export function dispatchMapPins( input ){
+    if (!Array.isArray(input)){
+        throw ArrayThrowException;
+    };
+    const mapPinsDispatch = {
+        type: 'mapPinsDispatch',
+        payload: input
+    };
+    rootStore.dispatch(mapPinsDispatch);
+}
+
+/**
+ * @brief This function retrieves and returns the stored array of map pins
+ */
+export function getMapPins(){
+    var state = rootStore.getState().mapPins;
+    return (state)
+}
