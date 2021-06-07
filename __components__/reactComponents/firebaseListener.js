28c1aec340c694e8183b34ec3b32971c8f659b3c
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2021-06-06 21:12:27 -0500

    updated to work with the redux package as is

--- __components__/reactComponents/firebaseListener.js
+++ __components__/reactComponents/firebaseListener.js
@@ -4,7 +4,7 @@ import { Alert } from 'react-native';
 import { useNetInfo } from '@react-native-community/netinfo';
 
 //necessary redux packages
-import {dispatchMapPins, getMapPins} from '../__redux__/Actions/REDUX_mapPins.js'; 
+import {dispatchMapPins} from '../__redux__/Actions/REDUX_mapPins.js'; 
 import {getZipcode} from '../__redux__/Actions/REDUX_zipcode';
 
 //firebase imports
@@ -15,38 +15,34 @@ import 'firebase/firestore';
 const firebaseConfig = {};
 firebase.initializeApp(firebaseConfig);
 var FireDB = firebase.firestore();
-
-
-const zipcode = getZipcode();
-const zipcodeRef = FireDB.collection("AppDown");
-const query = zipcodeRef.where('loc.Z', '==', zipcode).get(); //establish a promise reference to what is needed
 let runonce = false;
 
 /**
- * @brief This function will take ONE snapshot of our Database and push it to the redux store.
+ * @brief This function will take **ONE** snapshot of our Database and push it to the redux store.
  * @warning This function will have to be **called repeatedly to function as a listener**, as it internally uses a snapshot method to get the data from the server. As such, it should be called within an effect method of the calling component
  */
-function FirebaseDataSnapshot(){
+export async function FirebaseDataSnapshot(){
     const netinfo = useNetInfo();
     let dataPins = [];
     if (netinfo.isInternetReachable){   
-        query.then((qres)=>{
+        const zipcode = getZipcode();
+        const zipcodeRef = FireDB.collection("AppDown");
+        const query = zipcodeRef.where('zip', '==', zipcode).get();
+        await query; //DEBUG
+        query.then((qres)=>{ //testing needed to make sure that this callback runs before the component exits through the return. 
             if (qres.empty){
                 console.log("No pins for this zipcode found in DB.");
-                Alert.alert("Sorry, we were unable to download any pins for this zipcode from our database.");
+                Alert.alert("Sorry, we were unable to download any polling locations for this zipcode from our database.");
                 dispatchMapPins(dataPins);
                 return;
             }
-            const arr = qres.docChanges({includeMetadataChanges:true}); //array of DocumentChange objects. Each of these objects has the structure outlined here: https://firebase.google.com/docs/reference/js/firebase.firestore.DocumentChange
-            arr.map((obj)=>{ //go over all of the docchange objects
-                if (obj.type!='removed'){
-                    dataPins.push(obj.doc.data()); //push the data of each changed document that wasn't removed
-                }
+            qres.forEach((doc)=>{
+                dataPins.push(doc.data);
             });
+            dispatchMapPins(dataPins); 
         });
     }else if (!runonce){
         Alert.alert("Internet not reachable, we cannot render the pins")
         runonce = True; 
     }
-    return;
 }
