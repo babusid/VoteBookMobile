49bae24a00599e99a23c493d9a28c7e2a0ae390a
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2021-06-08 11:30:29 -0500

    added testserver config information directly into component file, added debug loggers

--- __components__/reactComponents/firebaseListener.js
+++ __components__/reactComponents/firebaseListener.js
@@ -4,7 +4,7 @@ import { Alert } from 'react-native';
 import { useNetInfo } from '@react-native-community/netinfo';
 
 //necessary redux packages
-import {dispatchMapPins} from '../__redux__/Actions/REDUX_mapPins.js'; 
+import {dispatchMapPins, getMapPins} from '../__redux__/Actions/REDUX_mapPins.js'; 
 import {getZipcode} from '../__redux__/Actions/REDUX_zipcode';
 
 //firebase imports
@@ -12,7 +12,15 @@ import firebase from 'firebase/app'
 import 'firebase/firestore';
 
 //firebase setup
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyBsO7neh6HPqucYs82viI7poY-MlNOQecA",
+  authDomain: "testserv-d3178.firebaseapp.com",
+  projectId: "testserv-d3178",
+  storageBucket: "testserv-d3178.appspot.com",
+  messagingSenderId: "76783832841",
+  appId: "1:76783832841:web:ae388e3441b8344b1a8dd8",
+  measurementId: "G-7VYFJVJJKT"
+};
 firebase.initializeApp(firebaseConfig);
 var FireDB = firebase.firestore();
 let runonce = false;
@@ -24,8 +32,12 @@ let runonce = false;
 export async function FirebaseDataSnapshot(){
     const netinfo = useNetInfo();
     let dataPins = [];
-    if (netinfo.isInternetReachable){   
-        const zipcode = getZipcode();
+    console.log(netinfo); //DEBUG
+    if(netinfo.isConnected){   
+        const zipcode = getZipcode().toString();
+        if (zipcode == undefined){
+            throw "Zipcode Undefined in Redux Store for Firebase Data Snapshot";
+        }
         const zipcodeRef = FireDB.collection("AppDown");
         const query = zipcodeRef.where('zip', '==', zipcode).get();
         await query; //DEBUG
@@ -37,12 +49,14 @@ export async function FirebaseDataSnapshot(){
                 return;
             }
             qres.forEach((doc)=>{
-                dataPins.push(doc.data);
+                dataPins.push(doc.data());
             });
+            console.log(dataPins); //DEBUG
             dispatchMapPins(dataPins); 
+            console.log(getMapPins()); //DEBUG
         });
     }else if (!runonce){
         Alert.alert("Internet not reachable, we cannot render the pins")
-        runonce = True; 
+        runonce = true; 
     }
 }
