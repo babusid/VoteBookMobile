119ccdfe071e81b17063b31c7b79b0adff84ffe6
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2021-06-09 21:06:11 -0500

    Made two versions of listener, one that is currently stable and should be used, one that we will likely migrate to later once it is completed

--- __components__/__reactComponents__/firebaseListener.js
+++ __components__/__reactComponents__/firebaseListener.js
@@ -11,6 +11,7 @@ import {getZipcode} from '../__redux__/Actions/REDUX_zipcode';
 import firebase from 'firebase/app'
 import 'firebase/firestore';
 
+
 //firebase setup
 const firebaseConfig = {
   apiKey: "AIzaSyBsO7neh6HPqucYs82viI7poY-MlNOQecA",
@@ -24,39 +25,81 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var FireDB = firebase.firestore();
 let runonce = false;
+let runonceExp = false;
 
 /**
  * @brief This function will take **ONE** snapshot of our Database and push it to the redux store.
  * @warning This function will have to be **called repeatedly to function as a listener**, as it internally uses a snapshot method to get the data from the server. As such, it should be called within an effect method of the calling component
+ * @param {import('@react-native-community/netinfo').NetInfoState} netinfo object from calling component to let snapshot know whether it should run at all
  */
-export async function FirebaseDataSnapshot(){
-    const netinfo = useNetInfo();
-    let dataPins = [];
-    console.log(netinfo); //DEBUG
-    if(netinfo.isConnected){   
+export async function FirebaseDataSnapshot(netinfo){
+    // const netinfo = useNetInfo();
+    // console.log(netinfo); //DEBUG
+    //const netinfo = {isConnected:true}; //DEBUG
+    if(netinfo.isConnected==true){  
+        console.log("here")
         const zipcode = getZipcode().toString();
         if (zipcode == undefined){
             throw "Zipcode Undefined in Redux Store for Firebase Data Snapshot";
         }
         const zipcodeRef = FireDB.collection("AppDown");
         const query = zipcodeRef.where('zip', '==', zipcode).get();
-        await query; //DEBUG
-        query.then((qres)=>{ //testing needed to make sure that this callback runs before the component exits through the return. 
-            if (qres.empty){
+        await query; 
+        query.then((qres)=>{
+            if (qres.empty){ //no pins to be stored
                 console.log("No pins for this zipcode found in DB.");
                 Alert.alert("Sorry, we were unable to download any polling locations for this zipcode from our database.");
-                dispatchMapPins(dataPins);
+                let blank = [];
+                dispatchMapPins(blank);
+                return ; //nothing important put into the store
+            }
+            if(qres.docs!= getMapPins()){ 
+                dispatchMapPins(qres.docs); 
                 return;
             }
-            qres.forEach((doc)=>{
-                dataPins.push(doc.data());
-            });
-            console.log(dataPins); //DEBUG
-            dispatchMapPins(dataPins); 
-            console.log(getMapPins()); //DEBUG
         });
     }else if (!runonce){
         Alert.alert("Internet not reachable, we cannot render the pins")
         runonce = true; 
+    }
+}
+
+
+/**
+ * @brief This function will take **ONE** snapshot of our Database and push it to the redux store.
+ * @warning This function will have to be **called repeatedly to function as a listener**, as it internally uses a snapshot method to get the data from the server. As such, it should be called within an effect method of the calling component
+ * @returns {Boolean} true or false depending on whether new data is pushed to the store
+ */
+ export async function FirebaseDataSnapshotExp(){
+    const netinfo = useNetInfo();
+    // console.log(netinfo); //DEBUG
+    if(netinfo.isConnected==true){  
+        const zipcode = getZipcode().toString();
+        if (zipcode == undefined){
+            throw "Zipcode Undefined in Redux Store for Firebase Data Snapshot";
+        }
+        const zipcodeRef = FireDB.collection("AppDown");
+        const query = zipcodeRef.where('zip', '==', zipcode).get();
+        await query; 
+        query.then((qres)=>{
+            if (qres.empty){ //no pins to be stored
+                console.log("No pins for this zipcode found in DB.");
+                Alert.alert("Sorry, we were unable to download any polling locations for this zipcode from our database.");
+                let blank = [];
+                dispatchMapPins(blank);
+                return false; //nothing important put into the store
+            }
+            if(getMapPins()==undefined||qres.docs!=undefined){ //there are no pins stored so we can just do a simple set
+                dispatchMapPins(qres.docs);
+                return true;
+            }
+            if(qres.docs!= getMapPins()){ 
+                dispatchMapPins(qres.docs); 
+                return true;
+            }
+        });
+    }else if (!runonceExp){
+        Alert.alert("Internet not reachable, we cannot render the pins")
+        runonceExp = true; 
     }
 }
