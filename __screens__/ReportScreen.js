efbe9c10503da93e07b966f8c5486ef1b1f5905e
William <hongwil123@gmail.com>
2021-06-14 10:27:57 -0500

    Created the buttons and initial setup, #CU-xxctw9[in progress]

--- __screens__/ReportScreen.js
+++ __screens__/ReportScreen.js
@@ -1,13 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import rootStore from "../__components__/__redux__/store.js"
+import { StyleSheet, Text, View, Button, Card, Alert } from 'react-native';
+import rootStore from "../__components__/__redux__/store.js";
+import { useNavigation} from '@react-navigation/native';
 
 
 export default function MapScreen(){
+    const navigation = useNavigation();
     return(
-        <View>
-            <Text>This is where the Reporting Screen should be exported from</Text>
-        </View>
+        <ScrollView>
+            <Card>
+            <View style={styles.container}>
+                <Button 
+                    style={styles.buttonStyle}
+                    title="15 Minutes"
+                    onPress={() => Alert.alert('15 Minutes Pressed')}
+                />
+                <Button 
+                    style={styles.buttonStyle}
+                    title="30 Minutes"
+                    onPress={() => Alert.alert('30 Minutes Pressed')}
+                />
+                <Button 
+                    style={styles.buttonStyle}
+                    title="45 Minutes"
+                    onPress={() => Alert.alert('45 Minutes Pressed')}
+                />
+                <Button 
+                    style={styles.buttonStyle}
+                    title="1 Hour"
+                    onPress={() => Alert.alert('1 Hour Pressed')}
+                />
+                <Button 
+                    style={styles.buttonStyle}
+                    title="1.5 Hours"
+                    onPress={() => Alert.alert('1.5 Hours Pressed')}
+                />
+                <Button 
+                    style={styles.buttonStyle}
+                    title="2 Hours"
+                    onPress={() => Alert.alert('2 Hours Pressed')}
+                />
+                <Button 
+                    style={styles.buttonStyle}
+                    title="2.5 Hours"
+                    onPress={() => Alert.alert('2.5 Hours Pressed')}
+                />
+                <Button 
+                    style={styles.buttonStyle}
+                    title="3 Hours"
+                    onPress={() => Alert.alert('3 Hours Pressed')}
+                />
+            </View>
+            </Card>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    button: {
+        padding: 15,
+    },
+});
