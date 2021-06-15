f5f22a2a9d243218225f9ddace98812511aa62cc
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2021-06-15 17:43:06 -0500

    made central firebase config file, initial setup of push function

--- __components__/__JScomponents__/firebaseListener.js
+++ __components__/__JScomponents__/firebaseListener.js
@@ -1,32 +1,9 @@
 //Imports
-import React from 'react';
 import { Alert } from 'react-native';
-import { useNetInfo } from '@react-native-community/netinfo';
-
 //necessary redux packages
-import {dispatchMapPins, getMapPins} from '../__redux__/Actions/REDUX_mapPins.js'; 
+import {dispatchMapPins} from '../__redux__/Actions/REDUX_mapPins.js'; 
 import {getZipcode} from '../__redux__/Actions/REDUX_zipcode';
-
-//firebase imports
-import firebase from 'firebase/app'
-import 'firebase/firestore';
-
-
-//firebase setup
-const firebaseConfig = {
-  apiKey: "AIzaSyBsO7neh6HPqucYs82viI7poY-MlNOQecA",
-  authDomain: "testserv-d3178.firebaseapp.com",
-  projectId: "testserv-d3178",
-  storageBucket: "testserv-d3178.appspot.com",
-  messagingSenderId: "76783832841",
-  appId: "1:76783832841:web:ae388e3441b8344b1a8dd8",
-  measurementId: "G-7VYFJVJJKT"
-};
-firebase.initializeApp(firebaseConfig);
-var FireDB = firebase.firestore();
-let runonce = false;
-let runonceExp = false;
-
+import { FireDB } from './firebaseConfig.js';
 /**
  * @brief This function will attach a listener to our database that will continually update our redux store as data changes on the server side
  * @param {import('@react-native-community/netinfo').NetInfoState} netinfo object from calling component to let snapshot know whether it should run at all
