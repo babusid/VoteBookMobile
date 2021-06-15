b1d021d4fca4bcefdd70ecf2c900ddd329a91c40
William <hongwil123@gmail.com>
2021-06-15 17:37:15 -0500

    Finished reportScreen #CU-xxctw9[review]

--- __screens__/ReportScreen.js
+++ __screens__/ReportScreen.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Card, Alert } from 'react-native';
-import { Button } from 'react-native-elements';
+import { StyleSheet, Text, View, Alert, ScrollView } from 'react-native';
+import { Button, Card } from 'react-native-elements';
 import rootStore from "../__components__/__redux__/store.js";
 import { useNavigation} from '@react-navigation/native';
 
@@ -12,39 +12,48 @@ export default function ReportScreen({ navigation }){
         <ScrollView>
             <Card>
             <View style={styles.container}>
-                <Text style={styles.container}>
+                <Text style={styles.text}>
                     Please select the estimated wait time for this polling location.
                 </Text>
-                <Button 
-                    style={styles.buttonStyle}
+                <View style={styles.button}>
+                  <Button 
+                    style={styles.button}
                     title="15 Minutes"
-                    onPress={() =>{Alert.alert("15 Minutes Pressed"), console.log("15 Minutes"), navigation.navigate("mapScreen")}}
+                    onPress={() =>{Alert.alert("15 Minutes Pressed"), console.log("15 Minutes")}}
                 />
+                <View style={styles.space} />
                 <Button 
-                    style={styles.buttonStyle}
+                    style={styles.button}
                     title="30 Minutes"
-                    onPress={() =>{Alert.alert("30 Minutes Pressed"), console.log("30 Minutes"), navigation.navigate("mapScreen")}}
+                    onPress={() =>{Alert.alert("30 Minutes Pressed"), console.log("30 Minutes")}}
                 />
-                <Button 
-                    style={styles.buttonStyle}
+                </View>
+                <View style={styles.button}>
+                  <Button 
+                    style={styles.button}
                     title="45 Minutes"
-                    onPress={() =>{Alert.alert("45 Minutes Pressed"), console.log("45 Minutes") navigation.navigate("mapScreen")}}
+                    onPress={() =>{Alert.alert("45 Minutes Pressed"), console.log("45 Minutes")}}
                 />
+                <View style={styles.space} />
                 <Button 
-                    style={styles.buttonStyle}
+                    style={styles.button}
                     title="1 Hour"
-                    onPress={() =>{Alert.alert("1 Hour Pressed"), console.log("1 Hour") navigation.navigate("mapScreen")}}
+                    onPress={() =>{Alert.alert("1 Hour Pressed"), console.log("1 Hour")}}
                 />
-                <Button 
-                    style={styles.buttonStyle}
+                </View>
+                <View style={styles.button}>
+                  <Button 
+                    style={styles.button}
                     title="1.5 Hours"
-                    onPress={() =>{Alert.alert("1.5 Hours Pressed"), console.log("1.5 Hours") navigation.navigate("mapScreen")}}
+                    onPress={() =>{Alert.alert("1.5 Hours Pressed"), console.log("1.5 Hours")}}
                 />
+                <View style={styles.space} />
                 <Button 
-                    style={styles.buttonStyle}
+                    style={styles.button}
                     title="2 Hours"
-                    onPress={() =>{Alert.alert("2 Hours Pressed"), console.log("2 Hours") navigation.navigate("mapScreen")}}
+                    onPress={() =>{Alert.alert("2 Hours Pressed"), console.log("2 Hours")}}
                 />
+                </View>
             </View>
             </Card>
         </ScrollView>
@@ -56,14 +65,20 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#FFFFFF',
         alignItems: 'center',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
     },
     text: {
         fontWeight: 'bold',
         fontSize: 20,
+        textAlign: 'center',
     },
     button: {
-        padding: 15,
+        paddingTop: 30,
+        paddingBottom: 30,
+        padding: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
     },
+    space: {
+        width: 250,
+    }
 });
