41075a9639f5ac1a25d6f3d1f9d32845acc45967
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2021-07-19 13:34:02 -0500

    Sample ballot planner #139jyxd[COMPLETE] (#35)
    * fixed a couple weird errors, and established basic framework for the sample ballot tool for ballot planner
    
    * Modularized sample ballot, updated documentation #139jyxd[REVIEW]

--- __screens__/BallotPlanner.js
+++ __screens__/BallotPlanner.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-
+import React, { useEffect } from 'react';
+import { ScrollView, Text } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { Linking } from 'react-native';
 
 /**
- * @brief - This function is the overall export of the entirety of the ballot planner. 
- * It takes the zip code stored in the redux store object, pings a database of national candidates sorted by zipcode and 
- * displays a drop-down for each position where the user can select a candidate. These selected candidates will be stored
- * to persistent storage, in the same way that the zip code will be from the login screen. This way, when the user opens the app 
- * repeatedly, their selections are persisted.
+ * @brief - This function is the overall export of the entirety of the ballot researcher tool.
+ * Currently, it renders only the sample Ballot tool pulled from BallotPedia. In the future it will be updated to be a nav menu hub screen 
+ * that displays other tools as well.
  */
 
 export default function BallotPlanner(){
-    return (
-        <View>
-            <Text>This is where the BallotPlanner Screen should be exported from</Text>
-        </View>
-    );
+    return(
+        <SampleBallot/>
+    )
+}
+
+/**
+ * 
+ * @returns {JSX.Element} This component will render a trimmed down version of the BallotPedia Ballot Planner website, that links out externally through the device browser
+ * to their website if any link is clicked.
+ */
+function SampleBallot(){
+    const INJECTED_JAVASCRIPT = `
+    let list = (document.querySelectorAll("*"));
+    for(let i=0;i<list.length;i++){
+      list[i].style.visibility="hidden";
+    }
+    list = (document.querySelectorAll("*.holds-the-iframe,#myIframe"));
+    for(let i=0;i<list.length;i++){
+      list[i].style.visibility="visible";
+    }
+    ;`
+
+    return(
+        <WebView 
+        source={{uri: "https://ballotpedia.org/Sample_Ballot_Lookup"}} 
+        onShouldStartLoadWithRequest={(event)=>{
+            if(event.mainDocumentURL!="https://ballotpedia.org/Sample_Ballot_Lookup"){
+                Linking.openURL(event.url);
+                return false;
+            }
+            return true;
+        }}
+        injectedJavaScript={INJECTED_JAVASCRIPT}
+        onMessage={(event)=>{
+            const dat = JSON.parse(event.nativeEvent.data);
+            console.log(dat);
+        }}
+        />
+    )
 }
